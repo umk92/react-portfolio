2dd4fd47bac4b3db7387cbf2ef5e0bd648fbd279
umk92 <uppuluri_m@yahoo.com>
2023-05-07 21:29:10 +0530

    modified change

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -1,12 +1,12 @@
 import {FaAward} from 'react-icons/fa'
 import {TbBooks} from 'react-icons/tb'
-// import {BiHappyHeartEyes} from 'react-icons/bi'
+// import {TbAddressBook} from 'react-icons/tb'
 
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '5+ Years Working'},
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '5+ Years working'},
     {id: 2, icon: <TbBooks/>, title: 'Skills', desc: 'HTML5, CSS3, Javascript, ReactJS'},
-    // {id: 3, icon: <BiHappyHeartEyes/>, title: 'Clients', desc: '70+ happy clients'}
+    // {id: 3, icon: <TbAddressBook/>, title: 'Education', desc: 'X Standard: 82.83% , Intermediate: 83.00%, B.Tech(ECE): 72.71%.'},
     ]
 
 
